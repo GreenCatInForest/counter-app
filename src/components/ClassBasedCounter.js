3e1cb0d6ece034551d0231fccdeb4da080be71de
GreenCatInForest <event2016.2017@gmail.com>
2023-11-06 01:09:16 +0000

    add onclick methods to buttons

--- src/components/ClassBasedCounter.js
+++ src/components/ClassBasedCounter.js
@@ -5,18 +5,33 @@ export class ClassBasedCounter extends Component {
     super(props);
   }
 
+  handleIncrement = () => {
+    console.log("handleIncrement");
+  };
+  handleReset = () => {};
+  handleDecrement = () => {};
+
   render() {
     return (
       <div className="border border-secondary m-3 p-4 rounded">
         <div className="text-center display-6 mb-4">Counter: 0</div>
         <div className="d-grid gap-2 col-6 mx-auto">
-          <button className="btn btn-success btn-lg display-3 mx-3">
+          <button
+            className="btn btn-success btn-lg"
+            onClick={this.handleIncrement}
+          >
             Increment
           </button>
-          <button className="btn btn-secondary btn-lg display-1 mx-3 ">
+          <button
+            className="btn btn-secondary btn-lg"
+            onClick={this.handleReset}
+          >
             Reset
           </button>
-          <button className="btn btn-danger btn-lg display-3 mx-3">
+          <button
+            className="btn btn-danger btn-lg"
+            onClick={this.handleDecrement}
+          >
             Decrement
           </button>
         </div>
