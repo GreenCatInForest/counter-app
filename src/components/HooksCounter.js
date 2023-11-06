dd0f2e68c1e5f84b21960067070c42b031d060d4
GreenCatInForest <event2016.2017@gmail.com>
2023-11-06 18:12:37 +0000

    add handleIncrement functionality set the value of count

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
 export const HooksCounter = () => {
+  // declare useState
+
+  const [count, setCount] = useState(0);
+
+  // buttons pointed to functions over here
+  const handleIncrement = () => {
+    // test the button connected to method
+    // console.log("handleIncrement");
+
+    // set the value of count to the current value of count+1
+    setCount(count + 1);
+  };
+
+  const handleReset = () => {};
+
+  const handleDecrement = () => {};
+
   return (
     <div className="border border-secondary m-3 p-4 rounded">
       <div className="text-center display-6 mb-4">
-        Functional Based Counter: 0
+        Functional Based Counter: {count}
       </div>
       <div className="d-grid gap-2 col-6 mx-auto">
-        <button className="btn btn-success btn-lg">Increment</button>
-        <button className="btn btn-secondary btn-lg">Reset</button>
-        <button className="btn btn-danger btn-lg">Decrement</button>
+        <button className="btn btn-success btn-lg" onClick={handleIncrement}>
+          Increment
+        </button>
+        <button className="btn btn-secondary btn-lg" onClick={handleReset}>
+          Reset
+        </button>
+        <button className="btn btn-danger btn-lg" onClick={handleDecrement}>
+          Decrement
+        </button>
       </div>
     </div>
   );
