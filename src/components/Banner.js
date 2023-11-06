98e77b66aea2c12ccb6ec1ce451cbdafaaee7395
GreenCatInForest <event2016.2017@gmail.com>
2023-11-06 00:17:29 +0000

    complete styled banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,14 @@
+import { Stack } from "react-bootstrap";
+
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <Stack className="container-fluid py-4 bg-dark ">
+      <h1 className="display-5 fw-normal py-3 text-center text-info">
+        React Counter
+      </h1>
+      <h2 className="text-center fw-light py-3 text-center text-light">
+        Please use the buttons to interact with your counter
+      </h2>
+    </Stack>
+  );
 };
